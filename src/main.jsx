d83a33310d4b6b564cac124317bcba66b5fb1874
Nestorfron <nestorfrones07@hotmail.com>
2025-01-04 12:00:31 -0300

    import { SpeedInsights } from deleted

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import {ThemeProvider as NextThemesProvider} from "next-themes";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import App from "./App.jsx";
 import "../src/styles/index.css";
@@ -11,7 +10,6 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <SpeedInsights />
         <App />
       </NextThemesProvider>
     </NextUIProvider>
